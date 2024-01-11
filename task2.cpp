72d86a750b31587ffa22c317dc2432c20e9876ac
GazetdinowaRR <64542397+GazetdinowaRR@users.noreply.github.com>
2024-01-11 06:39:39 +0400

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    std::ifstream circleFile("file1.txt");
+    std::ifstream pointsFile("file2.txt");
+
+    if (!circleFile.is_open() || !pointsFile.is_open()) {
+        std::cerr << "Failed to open input files." << std::endl;
+        return 1;
+    }
+
+    double centerX, centerY, radius;
+    circleFile >> centerX >> centerY >> radius;
+
+    double x, y;
+    while (pointsFile >> x >> y) {
+        double distance = hypot(x - centerX, y - centerY);
+        if (distance < radius) {
+            std::cout << "Точка (" << x << ", " << y << ") внутри окружности." << std::endl;
+        }
+        else if (distance == radius) {
+            std::cout << "Point (" << x << ", " << y << ") лежит на окружности." << std::endl;
+        }
+        else {
+            std::cout << "Point (" << x << ", " << y << ") снаружи окружности." << std::endl;
+        }
+    }
+
+    circleFile.close();
+    pointsFile.close();
+
+    return 0;
+}
---
+++ task4.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int minMoves(std::vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    int median = nums[nums.size() / 2];
+    int moves = 0;
+
+    for (int num : nums) {
+        moves += std::abs(num - median);
+    }
+
+    return moves;
+}
+
+int main() {
+    std::vector<int> nums;
+    int num;
+
+    std::cout << "Enter space-separated integers. Press Ctrl+D (or Ctrl+Z on Windows) when done:" << std::endl;
+    while (std::cin >> num) {
+        nums.push_back(num);
+    }
+
+    int moves = minMoves(nums);
+
+    std::cout << "Min moves: " << moves << std::endl;
+
+    return 0;
+}
